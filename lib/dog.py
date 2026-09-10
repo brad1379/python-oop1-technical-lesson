6e7031c5114340d10219447993c275872a9ff027
Brad Arnold <brad1379@gmail.com>
2026-09-09 19:17:26 -0500

    initialized class and methods

--- lib/dog.py
+++ lib/dog.py
@@ -1,1 +1,12 @@
-# Dog model goes here
+class Dog:
+    def __init__(self, name, breed, age, last_checkup=None):
+        self.name = name 
+        self.breed = breed
+        self.age = age
+        self.last_checkup = last_checkup
+
+    def checkup(self, date):
+        pass
+
+    def birthday_celebration(self):
+        pass
