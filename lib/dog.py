090e401bdaf764ec7bb8ce1d83b1c6bb05ddc795
Brad Arnold <brad1379@gmail.com>
2026-09-12 09:32:11 -0500

    Finish dog model

--- lib/dog.py
+++ lib/dog.py
@@ -6,7 +6,39 @@ class Dog:
         self.last_checkup = last_checkup
 
     def checkup(self, date):
-        pass
+        print(f"Checking up with {self.name} on {date}")
+        self.last_checkup = date
 
     def birthday_celebration(self):
-        pass
+        self.age += 1
+        print(f"{self.name} is turning {self.age}")
+
+    def get_age(self):
+        return self._age
+
+    def set_age(self, value):
+        if type(value) is int and 0 <= value:
+            self._age = value
+        else:
+            print("Not a valid age")
+
+    age = property(get_age, set_age)
+
+
+
+fido = Dog("Fido", "Golden Retriever", 3, "05/22/2022")
+clifford = Dog(
+    name = "Clifford",
+    age = 2,
+    breed = "Big Red"
+)
+
+print(fido.age)
+fido.birthday_celebration()
+print(fido.age)
+print(clifford.last_checkup)
+clifford.checkup("03/02/2024")
+print(clifford.last_checkup)
+
+balto = Dog("Balto", "Husky", "Not an age")
+steele = Dog("Steele", "Husky", -10)
